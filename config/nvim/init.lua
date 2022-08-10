881e2197202d5516cd76c7486de6510a397788dc
Paul Rosania <paul.rosania@gmail.com>
2022-08-09 20:21:17 -0700

    nvim: switch to packer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,60 +1,58 @@
-vim.call('plug#begin')
-local Plug = vim.fn['plug#']
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 
--- Many utilities
-Plug 'echasnovski/mini.nvim'
+  -- Many utilities
+  use 'echasnovski/mini.nvim'
 
--- Fuzzy find
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
-Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'})
+  -- Fuzzy find
+  -- Optional
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'tami5/sqlite.lua'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-frecency.nvim'
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  }
 
--- Git
-Plug 'airblade/vim-gitgutter'
+  -- Git
+  use 'airblade/vim-gitgutter'
 
--- Collection of common configurations for the Nvim LSP client
-Plug 'neovim/nvim-lspconfig'
-Plug 'jose-elias-alvarez/null-ls.nvim'
+  -- Collection of common configurations for the Nvim LSP client
+  use 'neovim/nvim-lspconfig'
+  use 'jose-elias-alvarez/null-ls.nvim'
 
--- Treesitter
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+  -- Treesitter
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
--- TypeScript
-Plug 'jose-elias-alvarez/nvim-lsp-ts-utils'
+  -- TypeScript
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
 
--- Completion framework
-Plug 'hrsh7th/nvim-cmp'
+  -- Completion framework
+  use 'hrsh7th/nvim-cmp'
 
--- LSP completion source for nvim-cmp
-Plug 'hrsh7th/cmp-nvim-lsp'
+  -- LSP completion source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp'
 
--- Snippet completion source for nvim-cmp
-Plug 'hrsh7th/cmp-vsnip'
+  -- Snippet completion source for nvim-cmp
+  use 'hrsh7th/cmp-vsnip'
 
--- Other useful completion sources
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-buffer'
+  -- Other useful completion sources
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
 
--- See hrsh7th's other plugins for more completion sources!
+  -- See hrsh7th's other plugins for more completion sources!
 
--- To enable more of the features of rust-analyzer, such as inlay hints and more!
-Plug 'simrat39/rust-tools.nvim'
+  -- To enable more of the features of rust-analyzer, such as inlay hints and more!
+  use 'simrat39/rust-tools.nvim'
 
--- Snippet engine
-Plug 'hrsh7th/vim-vsnip'
+  -- Snippet engine
+  use 'hrsh7th/vim-vsnip'
 
--- Fuzzy finder
--- Optional
-Plug 'nvim-lua/popup.nvim'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'tami5/sqlite.lua'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-telescope/telescope-frecency.nvim'
-
--- Color scheme used in the GIFs!
-Plug 'arcticicestudio/nord-vim'
-
-vim.call('plug#end')
+  -- Color scheme used in the GIFs!
+  use 'arcticicestudio/nord-vim'
+end)
 
 vim.opt.updatetime = 100
 
