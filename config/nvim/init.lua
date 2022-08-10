2f7cd08aebee10c6ae63f48fad04376914e54c16
Paul Rosania <paul.rosania@gmail.com>
2022-08-09 22:20:50 -0700

    nvim: extract LSP config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
 require('plugins')
+require('lsp')
 
 vim.opt.updatetime = 100
 
@@ -49,33 +50,6 @@ vim.opt.completeopt = 'menuone,noinsert,noselect'
 -- Avoid showing extra messages when using completion
 vim.opt.shortmess:append('c')
 
--- Configure LSP through rust-tools.nvim plugin.
--- rust-tools will configure and enable certain LSP features for us.
--- See https://github.com/simrat39/rust-tools.nvim#configuration
-
-local lspconfig = require('lspconfig')
-local null_ls = require('null-ls')
-
-local opts = {
-    -- rust-tools
-    tools = {
-        autoSetHints = true,
-    },
-
-    -- nvim-lspconfig
-    server = {
-        settings = {
-            ['rust-analyzer'] = {
-                checkOnSave = {
-                    command = 'clippy'
-                },
-            }
-        }
-    },
-}
-
-require('rust-tools').setup(opts)
-
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -84,83 +58,3 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', 'dm', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    -- vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    -- vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
-    -- vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
-    -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-    -- vim.keymap.set('i', '<C-x><C-x>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-  end
-end
-
-lspconfig.prismals.setup({})
-
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-})
-
-lspconfig.tsserver.setup({
-    on_attach = function(client, bufnr)
-        -- disable formatting, we'll use Prettier
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
-
-        local ts_utils = require("nvim-lsp-ts-utils")
-        ts_utils.setup({})
-        ts_utils.setup_client(client)
-
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gs', ':TSLspOrganize<CR>', bufopts)
-        vim.keymap.set('n', 'gi', ':TSLspRenameFile<CR>', bufopts)
-        vim.keymap.set('n', 'go', ':TSLspImportAll<CR>', bufopts)
-
-        on_attach(client, bufnr)
-    end,
-})
-
-null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.code_actions.eslint_d.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.formatting.prettier.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.formatting.sqlfluff.with({
-            extra_args = { "--dialect", "sqlite" },
-        }),
-    },
-    on_attach = on_attach
-})
---
+++ config/nvim/lua/lsp/init.lua
@@ -0,0 +1,100 @@
+local lspconfig = require('lspconfig')
+
+require('rust-tools').setup({
+    -- rust-tools
+    tools = {
+        autoSetHints = true,
+    },
+
+    -- nvim-lspconfig
+    server = {
+        settings = {
+            ['rust-analyzer'] = {
+                checkOnSave = {
+                    command = 'clippy'
+                },
+            }
+        }
+    },
+})
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    -- vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+    -- vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
+    -- vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+    -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+    -- vim.keymap.set('i', '<C-x><C-x>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  end
+end
+
+lspconfig.prismals.setup({})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+})
+
+lspconfig.tsserver.setup({
+    on_attach = function(client, bufnr)
+        -- disable formatting, we'll use Prettier
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+
+        local ts_utils = require("nvim-lsp-ts-utils")
+        ts_utils.setup({})
+        ts_utils.setup_client(client)
+
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gs', ':TSLspOrganize<CR>', bufopts)
+        vim.keymap.set('n', 'gi', ':TSLspRenameFile<CR>', bufopts)
+        vim.keymap.set('n', 'go', ':TSLspImportAll<CR>', bufopts)
+
+        on_attach(client, bufnr)
+    end,
+})
+
+local null_ls = require('null-ls')
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.eslint_d.with({
+            prefer_local = "node_modules/.bin",
+        }),
+        null_ls.builtins.code_actions.eslint_d.with({
+            prefer_local = "node_modules/.bin",
+        }),
+        null_ls.builtins.formatting.prettier.with({
+            prefer_local = "node_modules/.bin",
+        }),
+        null_ls.builtins.formatting.sqlfluff.with({
+            extra_args = { "--dialect", "sqlite" },
+        }),
+    },
+    on_attach = on_attach
+})
