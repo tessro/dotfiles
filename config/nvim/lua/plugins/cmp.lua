171edc61e98b31d7148dc805ca0499a14ce87426
Paul Rosania <paul.rosania@gmail.com>
2023-01-05 12:41:07 -0800

    nvim: remove unnecessary option, it is the default

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -21,10 +21,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
   },
 
   -- Installed sources
