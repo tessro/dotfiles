fb30bd7a3f6f785942fa4395fa7469346802ea3a
Paul Rosania <paul.rosania@gmail.com>
2023-01-05 12:41:28 -0800

    nvim: migrate <C-e> from cmp.close to cmp.abort

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -20,7 +20,7 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true })
   },
 
