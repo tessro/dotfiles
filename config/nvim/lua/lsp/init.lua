b5623c11970810799fc67e7fdfc168b9ce573ad4
Paul Rosania <paul.rosania@gmail.com>
2023-01-05 13:17:21 -0800

    nvim: formatting

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -2,32 +2,32 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 require('rust-tools').setup({
-    -- rust-tools
-    tools = {
-        autoSetHints = true,
-    },
+  -- rust-tools
+  tools = {
+    autoSetHints = true,
+  },
 
-    -- nvim-lspconfig
-    server = {
-        settings = {
-            ['rust-analyzer'] = {
-                checkOnSave = {
-                    command = 'clippy'
-                },
-            }
-        }
-    },
+  -- nvim-lspconfig
+  server = {
+    settings = {
+      ['rust-analyzer'] = {
+        checkOnSave = {
+          command = 'clippy'
+        },
+      }
+    }
+  },
 })
 
 -- this callback is invoked on attach, when the client supports formatting
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- disable tsserver-based formatting (in favor of Prettier via `null-ls`)
-            return client.name ~= "tsserver"
-        end,
-        bufnr = bufnr,
-    })
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- disable tsserver-based formatting (in favor of Prettier via `null-ls`)
+      return client.name ~= "tsserver"
+    end,
+    bufnr = bufnr,
+  })
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
@@ -42,12 +42,12 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    -- vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    -- vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
-    -- vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
-    -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-    -- vim.keymap.set('i', '<C-x><C-x>', vim.lsp.buf.signature_help, bufopts)
+  -- vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+  -- vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+  -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
+  -- vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+  -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+  -- vim.keymap.set('i', '<C-x><C-x>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -93,34 +93,34 @@ lspconfig.rust_analyzer.setup({
 })
 
 lspconfig.tsserver.setup({
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-        local ts_utils = require("nvim-lsp-ts-utils")
-        ts_utils.setup({})
-        ts_utils.setup_client(client)
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    local ts_utils = require("nvim-lsp-ts-utils")
+    ts_utils.setup({})
+    ts_utils.setup_client(client)
 
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gs', ':TSLspOrganize<CR>', bufopts)
-        vim.keymap.set('n', 'gi', ':TSLspRenameFile<CR>', bufopts)
-        vim.keymap.set('n', 'go', ':TSLspImportAll<CR>', bufopts)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gs', ':TSLspOrganize<CR>', bufopts)
+    vim.keymap.set('n', 'gi', ':TSLspRenameFile<CR>', bufopts)
+    vim.keymap.set('n', 'go', ':TSLspImportAll<CR>', bufopts)
 
-        on_attach(client, bufnr)
-    end,
+    on_attach(client, bufnr)
+  end,
 })
 
 local null_ls = require('null-ls')
 null_ls.setup({
-    capabilities = capabilities,
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.code_actions.eslint_d.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.formatting.prettier.with({
-            prefer_local = "node_modules/.bin",
-        }),
-    },
-    on_attach = on_attach
+  capabilities = capabilities,
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    null_ls.builtins.code_actions.eslint_d.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = "node_modules/.bin",
+    }),
+  },
+  on_attach = on_attach
 })
