5f0eb9f0923b32d6c6414f424b489823c0e4ef5b
Paul Rosania <paul.rosania@gmail.com>
2023-01-26 09:17:54 -0800

    nvim: remove comments that are implemented

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -43,11 +43,8 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  -- vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-  -- vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
   -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, bufopts)
   -- vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
-  -- vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
   -- vim.keymap.set('i', '<C-x><C-x>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
