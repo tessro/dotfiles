3a4ae3590edf965d400ed5b42d4f8d8c31b80e8a
Paul Rosania <paul.rosania@gmail.com>
2023-01-26 09:08:49 -0800

    nvim: include dotfiles in fuzzy find

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,14 @@ telescope.setup({
       sort_mru = true,
       ignore_current_buffer = true,
     },
+    find_files = {
+      -- include non-`.gitignore`d dotfiles
+      hidden = true,
+      -- explicitly ignore `.git`, since it is not in `.gitignore`
+      file_ignore_patterns = {
+        '^.git/',
+      },
+    },
     help_tags = {
       layout_strategy = 'vertical',
     },
